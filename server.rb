0e22b46b2287a2ea9a2fd546cfded008e574944f
Andrew Mione <mione.a@gmail.com>
2015-02-10 12:07:19 -0800

    getting closer

--- server.rb
+++ server.rb
@@ -29,7 +29,7 @@ end
 post "/upload" do
 	puts params
 	filepicker_blob = params
-	filepicker_url = filepicker_blob("filepicker_url")
+	filepicker_url = filepicker_blob["filepicker_url"]
 	db = SQLite3::Database.new("urls")
 	create = db.execute("create table urls(url TEXT);")
 	write = db.execute("insert into urls(url) values (#{filepicker_url})")
