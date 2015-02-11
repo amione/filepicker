77145d39e503193f763f8451cdfbe232b89394db
Andrew Mione <mione.a@gmail.com>
2015-02-10 16:28:15 -0800

    ids

--- server.rb
+++ server.rb
@@ -29,7 +29,7 @@ end
 post "/upload" do
 	puts params
 	filepicker_blob = params
-	filepicker_url = filepicker_blob["filepicker_url"].to_s
+	@filepicker_url = filepicker_blob["filepicker_url"].to_s
 	puts filepicker_url
 	db = SQLite3::Database.new("urls.db")
 	last_id = db.execute("select id from urls order by id desc")
